7f9dd12699818312c67017523c4d4369578df016
mansi Kumari <mannsi980k@gmail.com>
2024-04-20 20:50:34 +0530

    react app code added

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 // console.log(app);
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
   });
